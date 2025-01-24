eb44a51b236670b28f4e9c7d6006699cdf020b2b
rach-syahputra <rach.syahputra@gmail.com>
2025-01-24 17:53:44 +0800

    feat: update error response

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -17,11 +17,6 @@ export class App {
   }
 
   private routes() {
-    // this.app.use((req: Request, res: Response, next: NextFunction) => {
-    //   res.status(200).json({
-    //     message: 'Welcome to loket.com API'
-    //   })
-    // })
     this.app.use('/auth', authRoute)
   }
 
@@ -46,16 +41,19 @@ export class App {
 
         if (err instanceof ResponseError) {
           res.status(err.code || 500).json({
+            success: false,
             error: {
               message: err.message
             }
           })
         } else if (err instanceof ZodError) {
           res.status(400).json({
+            success: false,
             error: err.issues[0]
           })
         } else {
           res.status(500).json({
+            success: false,
             error: {
               message: err.message
             }
