d3d64324ddd201ef79beea0c270d8e91060f473a
rach-syahputra <rach.syahputra@gmail.com>
2025-02-01 08:39:17 +0800

    feat: implement session updating, jwt decoding and pages protection

--- apps/web/src/auth.ts
+++ apps/web/src/auth.ts
@@ -1,7 +1,11 @@
-import NextAuth from 'next-auth'
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import NextAuth, { Session } from 'next-auth'
+import { JWT } from 'next-auth/jwt'
 import Credentials from 'next-auth/providers/credentials'
+import jwt from 'jsonwebtoken'
 
 import { fetchLogin } from './lib/apis/auth.api'
+import { UserToken } from './lib/interfaces/auth.interface'
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   pages: {
@@ -19,26 +23,67 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
           password: credentials.password as string
         })
 
-        if (!user) return null
+        if (!user.success) return null
 
-        return user.data
+        const decoded = jwt.decode(user.data.accessToken) as UserToken
+
+        return {
+          user: {
+            accessToken: user.data.accessToken,
+            ...decoded
+          }
+        }
       }
     })
   ],
   callbacks: {
-    async jwt({ token, user }) {
+    async jwt({ token, user, trigger, session }) {
       if (user) {
-        token.accessToken = user.accessToken
+        token.accessToken = user.user.accessToken
+        token.id = user.user.id
+        token.name = user.user.name
+        token.email = user.user.email
+        token.image = user.user.image
+        token.roleId = user.user.roleId
+      }
+
+      if (trigger === 'update' && session?.user) {
+        token.name = session?.user.name
+        token.image = session?.user.image
       }
 
       return token
     },
-    async session({ session, token }) {
+    async session({ session, token }: { session: Session; token: JWT }) {
       if (token) {
-        session.accessToken = token.accessToken as string
+        session.user.accessToken = token.accessToken
+        session.user.id = token.id
+        session.user.name = token.name
+        session.user.email = token.email
+        session.user.image = token.image
+        session.user.roleId = token.roleId
       }
 
       return session
+    },
+    authorized({ auth, request }) {
+      const isCustomer = auth?.user.roleId === 1
+      const isEventOrganizer = auth?.user.roleId === 2
+      const isOnCustomerDashboardPage =
+        request.nextUrl.pathname.startsWith('/member/c')
+      const isOnEventOrganizerDashboardPage =
+        request.nextUrl.pathname.startsWith('/member/o')
+      const isOnLoginPage = request.nextUrl.pathname.startsWith('/login')
+
+      if (
+        (isOnCustomerDashboardPage && !isCustomer) ||
+        (isOnEventOrganizerDashboardPage && !isEventOrganizer) ||
+        ((isCustomer || isEventOrganizer) && isOnLoginPage)
+      ) {
+        return Response.redirect(new URL('/', request.nextUrl))
+      }
+
+      return true
     }
   }
 })
--- apps/web/src/next-auth.d.ts
+++ apps/web/src/next-auth.d.ts
@@ -1,16 +1,38 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import NextAuth from 'next-auth'
+import { JWT } from 'next-auth/jwt'
 
 declare module 'next-auth' {
   interface User {
-    accessToken: string
+    user: {
+      id: number
+      email: string
+      image: string | null
+      name: string
+      roleId: number
+      accessToken: string
+    }
   }
 
   interface Session {
-    accessToken: string
+    user: {
+      id: number
+      email: string
+      image: string | null
+      name: string
+      roleId: number
+      accessToken: string
+    }
   }
+}
 
+declare module 'next-auth/jwt' {
   interface JWT {
     accessToken: string
+    id: number
+    email: string
+    image: string | null
+    name: string
+    roleId: number
   }
 }
