7bb1ef203b13746662fbf654440894952c3f4212
rach-syahputra <rach.syahputra@gmail.com>
2025-02-02 16:06:16 +0800

    chore: update next config

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -2,14 +2,22 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   images: {
-    domains: [
-      'assets.loket.com',
-      'loket-production-sg.s3.ap-southeast-1.amazonaws.com'
-    ], // Add the domain here
+    // domains: [
+    //   'assets.loket.com',
+    //   'loket-production-sg.s3.ap-southeast-1.amazonaws.com'
+    // ],
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'res.cloudinary.com'
+      },
+      {
+        protocol: 'https',
+        hostname: 'assets.loket.com'
+      },
+      {
+        protocol: 'https',
+        hostname: 'loket-production-sg.s3.ap-southeast-1.amazonaws.com'
       }
     ]
   }
