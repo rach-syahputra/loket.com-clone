b059688fb3f3529a998a63bc7778d3449ed7471e
rach-syahputra <rach.syahputra@gmail.com>
2025-02-23 11:42:16 +0800

    feat(api): update cors

--- apps/api/src/config.ts
+++ apps/api/src/config.ts
@@ -9,8 +9,9 @@ config({ path: resolve(__dirname, `../${envFile}`), override: true })
 
 export const PORT = process.env.PORT || 8000
 export const corsOptions: CorsOptions = {
-  origin: ['http://localhost:3000', 'https://mini-loket.vercel.app'],
+  origin: ['https://mini-loket.vercel.app', 'http://localhost:3000'],
   allowedHeaders: ['Content-Type', 'Authorization'],
+  optionsSuccessStatus: 200,
   credentials: true
 }
 
